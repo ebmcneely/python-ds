00465aedae2ebae0d538cc76d8f2b7149084008c
Erin McNeely <ebmcneely@gmail.com>
2021-04-27 18:27:09 -0500

    03_finished

--- 03_last_element/last_element.py
+++ 03_last_element/last_element.py
@@ -1,3 +1,8 @@
+nums = [3,5,234,4,85,-23,4]
+no_nums = []
+empty = []
+
+
 def last_element(lst):
     """Return last item in list (None if list is empty.
     
@@ -7,3 +12,7 @@ def last_element(lst):
         >>> last_element([]) is None
         True
     """
+    if lst != empty:
+        return lst[-1]
+    else: 
+        return None
